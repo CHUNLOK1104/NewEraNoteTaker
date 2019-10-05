d382e5b8bbf1978b71250c8847251acbeabfa132
coffee <chunlok8764@gmail.com>
2019-10-05 12:33:56 +0800

    s

--- app/src/main/java/com/newera/example/notetaker/MainActivity.java
+++ app/src/main/java/com/newera/example/notetaker/MainActivity.java
@@ -3,12 +3,41 @@ package com.newera.example.notetaker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText edtUsername , edtPassword;
+    private Button btnLogin;
+    private String username = "s", password = " 4";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
+        setListeners();
+    }
+
+    private void findView(){
+        edtUsername = findViewById(R.id.edt_username);
+        edtPassword = findViewById(R.id.edt_password);
+        btnLogin = findViewById(R.id.btn_login);
+    }
+    private void setListeners(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputUsername = edtUsername.getText();
+                String inputPassword = edtUsername.getText();
+
+                if (inputUsername.equals(username) && inputPassword.equals(password)) {
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
