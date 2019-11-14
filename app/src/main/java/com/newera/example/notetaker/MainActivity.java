059d3251aa03d20e059c36fc580d6e89273ba33f
chunl <chunlok8764@gmail.com>
2019-11-14 21:27:26 +0800

    1

--- app/src/main/java/com/newera/example/notetaker/MainActivity.java
+++ app/src/main/java/com/newera/example/notetaker/MainActivity.java
@@ -10,40 +10,43 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText edtUsername , edtPassword ;
+    private EditText edtUsername, edtPassword;
     private Button btnLogin;
     private String username = "s", password = "4";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
         setListeners();
+    }
 
-        private void findViews() {
-            edtUsername = findViewById(R.id.edt_username);
-            edtPassword = findViewById(R.id.edt_password);
-            btnLogin = findViewById(R.id.btn_login);
-        }
+    private void findViews() {
+        edtUsername = findViewById(R.id.edt_username);
+        edtPassword = findViewById(R.id.edt_password);
+        btnLogin = findViewById(R.id.btn_login);
+    }
 
-        private void setListeners() {
+    private void setListeners() {
 
-            btnLogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                    String inputUsername = edtUsername.getText().toString();
-                    String inputPassword = edtUsername.getText().toString();
+                String inputUsername = edtUsername.getText().toString();
+                String inputPassword = edtPassword.getText().toString();
 
-                    if (inputUsername.equals(username) && inputPassword.equals(password)) {
-                        //               Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                        Intent i = new Intent(MainActivity.this, HomeActivity.class);
-                        startActivity(i);
-                        finish();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
-                    }
+                if (inputUsername.equals(username) && inputPassword.equals(password)) {
+                    //               Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
                 }
-            });
-        }
+            }
+        });
+    }
+}
 
